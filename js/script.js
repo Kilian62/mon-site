8c1e32ff4d0e7bce412736bb543a00d26452e980
kilian <dabovalkilian@gmail.com>
2026-01-18 23:46:21 +0100

    Ajouts d'un template navbar

---
+++ js/script.js
@@ -0,0 +1,58 @@
+class maNavbar extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML =
+        `
+        <style>
+            nav {
+            font-size: 30px;
+            }
+            ul {
+                position: fixed;
+                display: flex;
+                justify-content: center;
+                width: 97%;
+                padding: 10px;
+                list-style-type: none;
+                overflow: hidden;
+
+                /* From https://css.glass */
+                background: rgba(255, 255, 255, 0);
+                border-radius: 16px;
+                box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+                backdrop-filter: blur(6.1px);
+                -webkit-backdrop-filter: blur(6.1px);
+                border: 1px solid rgba(255, 255, 255, 0.1);
+                border-radius: 100px;
+            }
+
+            ul li {
+            float: left;
+            
+            }
+
+            ul li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            
+            }
+        </style>
+        <nav>
+        <ul>
+            <li>
+                <a href="/pages/mes-projets.html">Mes Projets</a>
+            </li>
+            <li>
+                <a>Moi</a>
+            </li>
+            <li>
+                <a href="/index.html">Accueil</a>
+            </li>
+        </ul>
+    </nav>
+        `;
+    }
+}
+customElements.define('ma-navbar', maNavbar);
